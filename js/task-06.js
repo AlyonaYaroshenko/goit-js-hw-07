bf5eab79b0dd59a96e0d62e5ed39aa5205730ef2
AlyonaYaroshenko <alyona.yaroshenko03@gmail.com>
2021-05-02 23:06:27 +0300

    finished task 6

---
+++ js/task-06.js
@@ -0,0 +1,35 @@
+// Напиши скрипт, который бы при потере фокуса на инпуте, 
+// проверял его содержимое на правильное 
+// количество символов.
+// Сколько символов должно быть в инпуте, 
+// указывается в его атрибуте data - length.
+// // Если введено подходящее количество, 
+// то border инпута становится зеленым,
+// если неправильное - красным.
+// Для добавления стилей, используй 
+// CSS - классы valid и invalid.
+
+
+const input = document.querySelector('#validation-input');
+input.addEventListener('blur', onInputBlur);
+input.classList.add("js-valid", "js-invalid");
+const style = document.querySelector('.js-valid')
+const validLength = input.dataset.length;
+
+
+function onInputBlur() {
+    console.log('there is no focus');
+    if (input.value.length === Number(validLength)) {
+document.querySelector('.js-valid').style.borderColor = 'green';
+    }
+    else {
+document.querySelector('.js-invalid').style.borderColor = 'red';
+        };
+    
+}
+
+// function onInputGreen() {
+//     input.classList.add("js-valid") {
+
+//     }
+// }
