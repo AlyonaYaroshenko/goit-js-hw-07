7599cce0239aa0a83ed08fef7ebd3733c9387a54
AlyonaYaroshenko <alyona.yaroshenko03@gmail.com>
2021-05-03 01:11:49 +0300

    finished task 8

---
+++ js/task-08.js
@@ -0,0 +1,40 @@
+// Напиши скрипт создания и очистки коллекции элементов.
+// Пользователь вводит количество элементов в input и
+// нажимает кнопку Создать, после чего рендерится
+// коллекция.При нажатии на кнопку Очистить, коллекция
+// элементов очищается.
+// Создай функцию createBoxes(amount), которая принимает
+// 1 параметр amount - число.Функция создает столько div,
+// сколько указано в amount и добавляет их
+// в div#boxes.
+// Каждый созданный div:
+// имеет случайный rgb цвет фона
+// Размеры самого первого div - 30px на 30px
+// Каждый следующий div после первого, должен быть
+// шире и выше предыдущего на 10px
+// Создай функцию destroyBoxes(), которая очищает
+// div#boxes.
+
+const buttonRender = document.querySelector('[data-action="render"]');
+const buttonDestroy = document.querySelector('[data-action="destroy"]');
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('input');
+
+buttonRender.addEventListener('click', createBoxes);
+buttonDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+    amount = input.value;
+    const newPlace = [];
+    for (let i = 1; i <= amount; i += 1) {
+        const newDiv = document.innerHTML = `<div></div>`;
+    
+    newPlace.push(newDiv);
+    }
+    boxes.insertAdjacentHTML('afterbegin', newPlace.join(''));
+};
+
+function destroyBoxes() {
+    const removal = boxes.children;
+    [...removal].forEach(child => child.remove());
+}
